2ef155478582d3eaec592ebd72625fb30552e1f6
Nolan Robinson <fawxen@gmail.com>
2016-07-27 12:30:14 -0500

    move all task methods into manager.tasks, as that's where we are managing our tasks now.

--- role.builder.js
+++ role.builder.js
@@ -3,85 +3,14 @@ var managerTasks = require('manager.tasks');
 var roleBuilder = {
 
     run: function(creep) {
-        this._determineTask(creep);
-    },
-
-    _determineTask: function(creep) {
-        if(creep.memory.task == 'harvest' && creep.carry.energy < creep.carryCapacity) {
-            this._harvest(creep);
+        if(creep.memory.task == 'harvest') {
+            managerTasks.harvest(creep);
         }
         else if(creep.memory.task == 'build') {
-            this._buildStructures(creep);
+            managerTasks.build(creep);
         }
         else {
-            this._upgrade(creep);
-        }
-    },
-
-    _harvest: function(creep) {
-        var sources = creep.room.find(FIND_SOURCES);
-    
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
-        }
-    },
-
-    _buildStructures: function(creep) {
-        var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
-            filter: (structure) => {
-                return (//structure.structureType == STRUCTURE_STORAGE ||
-                        structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_WALL ||
-                        structure.structureType == STRUCTURE_ROAD);
-            }
-        });
-
-        if(targets.length) {
-            if(creep.carry.energy > 0) {
-                creep.memory.task = 'build';
-                
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            } else {
-                creep.memory.task = 'harvest';
-            }
-        } else {
-            creep.memory.task = 'upgrade';
-        }
-    },
-
-    _repairStructures: function(creep) {
-        var targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (
-                    structure.structureType == STRUCTURE_ROAD && structure.hits < 2000 ||
-                    structure.structureType == STRUCTURE_WALL && structure.hits < 50000);
-            }
-        });
-
-        if(creep.carry.energy > 0) {
-            if(targets.length) {
-                creep.memory.task = 'repair';
-                
-                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            } else {
-                creep.memory.task = 'build';
-            }
-        } else {
-            creep.memory.task = 'harvest';
-        }
-    },
-    
-    _upgrade: function(creep) {
-        if(creep.carry.energy > 0) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        } else {
-            creep.memory.task = 'harvest';
+            managerTasks.upgrade(creep);
         }
     }
 };

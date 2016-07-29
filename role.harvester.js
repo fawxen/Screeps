6ab9313d860d81c4c2818c72a1c0f31be7c930b7
Nolan Robinson <fawxen@gmail.com>
2016-07-29 12:26:47 -0500

    now uses the manager.tasks module to determine the creeps task

--- role.harvester.js
+++ role.harvester.js
@@ -1,49 +1,16 @@
+var managerTasks = require('manager.tasks');
+
 var roleHarvester = {
 
     run: function(creep) {
-        if(creep.memory.harvesting) {
-            if(creep.carry.energy < creep.carryCapacity && !creep.memory.storing) {
-                var sources = creep.room.find(FIND_SOURCES);
-                
-                if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[1]);
-                }
-                
-                creep.memory.harvesting = true;
-            }
-            else {
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-                });
-                
-                if(targets.length > 0) {
-                    creep.memory.storing = true;
-
-                    if(creep.carry.energy > 0) {
-                        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(targets[0]);
-                        }
-                    } else {
-                        creep.memory.storing = false;
-                    }
-                } else {
-                    creep.memory.harvesting = false;
-                }
-            }
+        if(creep.memory.task == 'harvest') {
+            managerTasks.harvest(creep, 'deposit', 1);
+        }
+        else if(creep.memory.task == 'deposit') {
+            managerTasks.build(creep, 'upgrade');
         }
         else {
-            if(creep.carry.energy > 0) {
-                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
-                }
-            }
-            else {
-                creep.memory.harvesting = true;
-            }
+            managerTasks.upgrade(creep);
         }
     }
 };
